8e26b64b083769ad32fd5f8d50c6cc0c41dbc853
mkitexist <68556353+mkitexist@users.noreply.github.com>
2021-01-26 09:12:04 +0530

    Updates

---
+++ precache-manifest.bae4e307c863dc41ee8a170478f9d199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9038b7ea74c4028e9bbace6708c2d0",
+    "url": "/travelinterface/index.html"
+  },
+  {
+    "revision": "b45b3da7f1eaa04be816",
+    "url": "/travelinterface/static/css/main.6a9f3f18.chunk.css"
+  },
+  {
+    "revision": "803205cd95dd07a59c68",
+    "url": "/travelinterface/static/js/2.bc9a7ce4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/travelinterface/static/js/2.bc9a7ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45b3da7f1eaa04be816",
+    "url": "/travelinterface/static/js/main.e5c18124.chunk.js"
+  },
+  {
+    "revision": "ce3c9518c7de880dc8c7",
+    "url": "/travelinterface/static/js/runtime-main.4aaf03a4.js"
+  }
+]);
